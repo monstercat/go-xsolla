cfeda70aa91285ffe7a55de8a085575fef203753
Thomas Mathews <thomas.c.mathews@gmail.com>
2021-07-15 11:39:20 -0700

    First strike.

---
+++ client.go
@@ -0,0 +1,172 @@
+package xsolla
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Client struct {
+	MerchantId     int
+	MerchantSecret string
+	ProjectId      int
+	ProjectSecret  string
+	Sandbox        bool
+	Timeout        time.Duration
+}
+
+func (c *Client) doReq(req *http.Request, out interface{}) error {
+	req.SetBasicAuth(strconv.Itoa(c.MerchantId), c.MerchantSecret)
+	req.Header.Set("Accept", "application/json; charset=UTF-8")
+	client := http.Client{
+		Timeout: c.Timeout,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	isJSON := strings.Contains(res.Header.Get("Content-Type"), "json")
+	body, err := io.ReadAll(res.Body)
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		err := RequestError{Raw: string(body)}
+		if isJSON {
+			json.Unmarshal(body, &err)
+		}
+		return &err
+	} else if isJSON {
+		return json.Unmarshal(body, out)
+	}
+	return nil
+}
+
+func (c *Client) newMerchantEndpoint(pathname string) *url.URL {
+	u, err := url.Parse(path.Join(fmt.Sprintf("%s/%d", EndpointMerchant, c.MerchantId), pathname))
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+
+func (c *Client) newProjectEndpoint(pathname string) *url.URL {
+	u, err := url.Parse(path.Join(fmt.Sprintf("%s/%d", EndpointProject, c.ProjectId), pathname))
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+
+// I am not sure about this style, I don't think project should be part of the main client and doing things like this.
+// It would be better to find a better way to do newRequest by just passing the url.URL in directly instead. For now
+// I am doing it like this to reduce current code verbosity.
+//
+// Secondly we are using panics because all request creations shouldn't fail during testing. If it panics there is
+// something wrong with the codebase.
+func (c *Client) newRequest(endpoint, method, pathname string, body io.Reader) *http.Request {
+	var u *url.URL
+	switch endpoint {
+	case EndpointMerchant:
+		u = c.newMerchantEndpoint(pathname)
+	case EndpointProject:
+		u = c.newProjectEndpoint(pathname)
+	default:
+		panic("no valid endpoint!")
+	}
+	req, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		panic(err)
+	}
+	return req
+}
+
+func (c *Client) newJSONRequest(endpoint, method, pathname string, body interface{}) (*http.Request, error) {
+	buf, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	req := c.newRequest(endpoint, method, pathname, bytes.NewReader(buf))
+	req.Header.Set("Content-Type", "application/json")
+	return req, nil
+}
+
+func (c *Client) NewTokenSettings() TokenSettings {
+	var mode string
+	if c.Sandbox {
+		mode = modeSandbox
+	}
+	return TokenSettings{
+		ProjectId: c.ProjectId,
+		Mode:      mode,
+		UI:        NewUISettings(),
+	}
+}
+
+func (c *Client) GetSubscriptionUserId(id string) (string, error) {
+	req := c.newRequest(EndpointMerchant, http.MethodGet, fmt.Sprintf("subscriptions/%s", id), nil)
+	var resPayload struct {
+		User struct {
+			Id string `json:"id"`
+		} `json:"user"`
+	}
+	err := c.doReq(req, &resPayload)
+	return resPayload.User.Id, err
+}
+
+func (c *Client) GetSubscriptionStatus(id int) (string, error) {
+	req := c.newRequest(EndpointProject, http.MethodGet, fmt.Sprintf("subscriptions/%d", id), nil)
+	var resPayload struct {
+		Status string `json:"status"`
+	}
+	if err := c.doReq(req, &resPayload); err != nil {
+		return "", err
+	}
+	return resPayload.Status, nil
+}
+
+func (c *Client) GetUser(userId string) (*User, error) {
+	req := c.newRequest(EndpointProject, http.MethodGet, fmt.Sprintf("users/%s", userId), nil)
+	var resPayload User
+	if err := c.doReq(req, &resPayload); err != nil {
+		return nil, err
+	}
+	return &resPayload, nil
+}
+
+func (c *Client) GetTransaction(id string) (*Transaction, error) {
+	req := c.newRequest(EndpointMerchant, http.MethodGet, fmt.Sprintf("reports/transactions/%s/details", id), nil)
+	var resPayload Transaction
+	if err := c.doReq(req, &resPayload); err != nil {
+		return nil, err
+	}
+	return &resPayload, nil
+}
+
+func (c *Client) CreateToken(token *Token) (string, error) {
+	req, err := c.newJSONRequest(EndpointMerchant, http.MethodPost, "token", token)
+	if err != nil {
+		return "", err
+	}
+	var resPayload struct {
+		Token string `json:"token"`
+	}
+	err = c.doReq(req, &resPayload)
+	return resPayload.Token, err
+}
+
+func (c *Client) CreateUserAttribute(attribute M) (int, error) {
+	req, err := c.newJSONRequest(EndpointProject, http.MethodPost, "user_attributes", attribute)
+	if err != nil {
+		return 0, err
+	}
+	var resPayload struct {
+		Id int `json:"id"`
+	}
+	err = c.doReq(req, &resPayload)
+	return resPayload.Id, err
+}
---
+++ xsolla.go
@@ -0,0 +1,195 @@
+package xsolla
+
+import (
+	"time"
+)
+
+const (
+	EndpointMerchant = "https://api.xsolla.com/merchant/v2/merchants"
+	EndpointProject  = "https://api.xsolla.com/merchant/v2/projects"
+
+	modeSandbox = "sandbox"
+)
+
+type RequestError struct {
+	Raw       string `json:"-"`
+	Code      int    `json:"http_status_code"`
+	Message   string `json:"message"`
+	RequestId string `json:"request_id"`
+}
+
+func (e *RequestError) Error() string {
+	if e.Message != "" {
+		return e.Message
+	} else if e.Raw != "" {
+		return e.Raw
+	}
+	return "empty xsolla request error"
+}
+
+type M map[string]interface{}
+
+type Webhook struct {
+	CustomParams     M            `json:"custom_parameters"`
+	NotificationType string       `json:"notification_type"`
+	PaymentDetails   M            `json:"payment_details"`
+	Purchase         M            `json:"purchase"`
+	Refund           M            `json:"refund_details"`
+	Subscription     Subscription `json:"subscription"`
+	Transaction      M            `json:"transaction"`
+	User             User         `json:"user"`
+}
+
+type User struct {
+	Country string `json:"country"`
+	Email   string `json:"email"`
+	Id      string `json:"id"`
+	IP      string `json:"ip"`
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Zip     string `json:"zip"`
+}
+
+// This is the subscription returned from webhooks
+type Subscription struct {
+	DateCreate     time.Time `json:"date_create"`
+	DateEnd        time.Time `json:"date_end"`
+	DateNextCharge time.Time `json:"date_next_charge"`
+	Id             int       `json:"subscription_id"`
+	PlanId         string    `json:"plan_id"`
+	ProductId      string    `json:"product_id"`
+	Tags           []string  `json:"tags"`
+	Trial          Trial     `json:"trial"`
+	User           User      `json:"user"`
+}
+
+type Trial struct {
+	Type  string `json:"type"`
+	Value int    `json:"value"`
+}
+
+type TokenSettings struct {
+	ProjectId int                    `json:"project_id"`
+	UI        map[string]interface{} `json:"ui"`
+	Mode      string                 `json:"mode,omitempty"`
+	ReturnURL string                 `json:"return_url,omitempty"`
+}
+
+type Token struct {
+	User         M                 `json:"user"`
+	CustomParams M                 `json:"custom_parameters,omitempty"`
+	Settings     TokenSettings     `json:"settings"`
+	Purchase     *PurchaseSettings `json:"purchase,omitempty"`
+}
+
+type PurchaseSettings struct {
+	Subscription PurchaseSubscription `json:"subscription"`
+	Description  PurchaseDescription  `json:"description"`
+	CouponCode   *PurchaseCouponCode  `json:"coupon_code,omitempty"`
+}
+
+type PurchaseDescription struct {
+	Value string `json:"value"`
+}
+
+type PurchaseCouponCode struct {
+	Value string `json:"value"`
+}
+
+type PurchaseSubscription struct {
+	PlanId    string `json:"plan_id,omitempty"`
+	ProductId string `json:"product_id,omitempty"`
+
+	// Subscription plans (array) to show in the payment UI.
+	AvailablePlans []string `json:"available_plans,omitempty"`
+
+	// The type of operation applied to the user’s subscription plan. To change the subscription plan, pass the
+	// change_plan value. You need to specify the new plan ID in the purchase.subscription.plan_id parameter.
+	Operation string `json:"operation,omitempty"`
+
+	// Currency of the subscription plan to use in all calculations.
+	Currency string `json:"currency,omitempty"`
+
+	// Trial period in days.
+	TrialDays int `json:"trial_days,omitempty"`
+}
+
+type TransactionSubscriptionDetails struct {
+	IsPaymentFromSubscription bool `json:"is_payment_from_subscription"`
+	IsSubscriptionCreated     bool `json:"is_subscription_created"`
+}
+
+type Transaction struct {
+	CustomerDetails     map[string]interface{}         `json:"customer_details"`
+	TransactionDetails  map[string]interface{}         `json:"transaction_details"`
+	SubscriptionDetails TransactionSubscriptionDetails `json:"subscription_details"`
+}
+
+func NewUserData(id, email, promo string, utm, attr M) M {
+	data := M{
+		"id": M{
+			"value":  id,
+			"hidden": true,
+		},
+		"email": M{
+			"value": email,
+		},
+		"country": M{
+			"allow_modify": true,
+		},
+	}
+
+	if promo != "" {
+		attr["promo"] = promo
+	}
+
+	if len(attr) > 0 {
+		data["attributes"] = attr
+	}
+
+	if len(utm) > 0 {
+		data["utm"] = utm
+	}
+
+	return data
+}
+
+func NewCustomParams(active, reg time.Time, tf bool) M {
+	return M{
+		"registration_date":       reg,
+		"active_date":             active,
+		"additional_verification": tf,
+	}
+}
+
+func NewUISettings() M {
+	return M{
+		"version": "desktop",
+		"desktop": M{
+			"header": M{
+				"type":         "compact",
+				"visible_name": true,
+			},
+			"subscription_list": M{},
+		},
+		"mobile": M{
+			"footer": M{
+				"is_visible": false,
+			},
+		},
+		"components": M{
+			"virtual_currency": M{
+				"hidden": true,
+			},
+		},
+	}
+}
+
+func NewUTM(source, campaign, term, content string) M {
+	return M{
+		"utm_source":   source,
+		"utm_campaign": campaign,
+		"utm_term":     term,
+		"utm_content":  content,
+	}
+}
