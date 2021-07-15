92921252d6a4d84213003f024e98b0394fac5109
Thomas Mathews <thomas.c.mathews@gmail.com>
2021-07-15 13:51:49 -0700

    Improved webhooks functionality.

--- client.go
+++ client.go
@@ -34,6 +34,8 @@ func (c *Client) doReq(req *http.Request, out interface{}) error {
 	}
 	isJSON := strings.Contains(res.Header.Get("Content-Type"), "json")
 	body, err := io.ReadAll(res.Body)
+	// All of Xsollas non-error responses should be within the 200 range.
+	// https://developers.xsolla.com/api/v2/getting-started/#api_errors_handling
 	if res.StatusCode < 200 || res.StatusCode >= 300 {
 		err := RequestError{Raw: string(body)}
 		if isJSON {
@@ -118,15 +120,13 @@ func (c *Client) GetSubscriptionUserId(id string) (string, error) {
 	return resPayload.User.Id, err
 }
 
-func (c *Client) GetSubscriptionStatus(id int) (string, error) {
+func (c *Client) GetSubscription(id int) (*Subscription, error) {
 	req := c.newRequest(EndpointProject, http.MethodGet, fmt.Sprintf("subscriptions/%d", id), nil)
-	var resPayload struct {
-		Status string `json:"status"`
-	}
+	var resPayload Subscription
 	if err := c.doReq(req, &resPayload); err != nil {
-		return "", err
+		return nil, err
 	}
-	return resPayload.Status, nil
+	return &resPayload, nil
 }
 
 func (c *Client) GetUser(userId string) (*User, error) {
--- webhook.go
+++ webhook.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 var (
@@ -15,27 +16,71 @@ var (
 )
 
 const (
-	NotificationTypePayment            = "payment"
-	NotificationTypeUserValidation     = "user_validation"
-	NotificationTypeCreateSubscription = "create_subscription"
-	NotificationTypeUpdateSubscription = "update_subscription"
-	NotificationTypeCancelSubscription = "cancel_subscription"
+	// https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_webhooks_list
+	NotificationTypeAFSBlackList           = "afs_black_list"
+	NotificationTypeAFSReject              = "afs_reject"
+	NotificationTypeCancelSubscription     = "cancel_subscription"
+	NotificationTypeCreateSubscription     = "create_subscription"
+	NotificationTypeGetPinCode             = "get_pincode"
+	NotificationTypeNonRenewalSubscription = "non_renewal_subscription"
+	NotificationTypePayment                = "payment"
+	NotificationTypePaymentAccountAdd      = "payment_account_add"
+	NotificationTypePaymentAccountRemove   = "payment_account_remove"
+	NotificationTypeRedeemKey              = "redeem_key"
+	NotificationTypeRefund                 = "refund"
+	NotificationTypeUpdateSubscription     = "update_subscription"
+	NotificationTypeUpgradeRefund          = "upgrade_refund"
+	NotificationTypeUserBalanceOperation   = "user_balance_operation"
+	NotificationTypeUserSearch             = "user_search"
+	NotificationTypeUserValidation         = "user_validation"
+
+	// https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_errors
+	WebhookErrCodeInvalidUser      = "INVALID_USER"
+	WebhookErrCodeInvalidParameter = "INVALID_PARAMETER"
+	WebhookErrCodeInvalidSignature = "INVALID_SIGNATURE"
+	WebhookErrCodeIncorrectAmount  = "INCORRECT_AMOUNT"
+	WebhookErrCodeIncorrectInvoice = "INCORRECT_INVOICE"
 )
 
+// Currently we use one structure for all webhook payloads. This makes it a bit confusing as to which webhooks have
+// certain fields - we may want to change this or simply add documentation to make it more clear to the develop what they
+// can expect.
 type Webhook struct {
 	// Raw is the pure string response of the webhook parsed from the HTTP request.
 	Raw string `json:"-"`
 
-	CustomParams     M            `json:"custom_parameters"`
-	NotificationType string       `json:"notification_type"`
-	PaymentDetails   M            `json:"payment_details"`
-	Purchase         M            `json:"purchase"`
-	Refund           M            `json:"refund_details"`
-	Subscription     Subscription `json:"subscription"`
-	Transaction      M            `json:"transaction"`
-	User             User         `json:"user"`
+	CustomParams     M                   `json:"custom_parameters"`
+	NotificationType string              `json:"notification_type"`
+	PaymentDetails   M                   `json:"payment_details"`
+	Purchase         M                   `json:"purchase"`
+	RefundDetails    M                   `json:"refund_details"`
+	Subscription     WebhookSubscription `json:"subscription"`
+	Transaction      M                   `json:"transaction"`
+	User             User                `json:"user"`
 }
 
+// The webhook subscription structure differs than the regular API structure, for who knows what reason.
+// https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_created_subscription
+// https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_canceled_subscription
+// https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_updated_subscription
+type WebhookSubscription struct {
+	DateCreate     time.Time `json:"date_create"`
+	DateEnd        time.Time `json:"date_end"`
+	// Only available in the updated & created notifications
+	DateNextCharge time.Time `json:"date_next_charge"`
+	SubscriptionId int       `json:"subscription_id"`
+	PlanId         string    `json:"plan_id"`
+	ProductId      string    `json:"product_id"`
+	Tags           []string  `json:"tags"`
+	Trial          Trial     `json:"trial"`
+}
+
+type WebhookError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+// https://developers.xsolla.com/api/v2/getting-started/#api_webhooks_signing_requests
 func ParseWebhook(req *http.Request, projectSecret string) (*Webhook, error) {
 	signature := strings.Replace(req.Header.Get("Authorization"), "Signature ", "", 1)
 	buf, err := ioutil.ReadAll(req.Body)
